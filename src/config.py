37187f1d019bcff07f2068b3aa3dd9645a8a5e19
Oluyale Ezekiel <amusecode@gmail.com>
2026-06-21 23:30:58 +0100

    feat: add LLM and VectorStore configuration

---
+++ src/config.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
+
+# Load environment variables globally
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    vertexai=True,
+    temperature=0,
+    )
+
+# Vector Database & Retriever
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="text-embedding-004",
+    vertexai=True
+)
+
+pc = Pinecone()
+index = pc.Index('rag')
+vectorstore = PineconeVectorStore(
+    index=index, 
+    embedding=embeddings
+)
+
+retriever = vectorstore.as_retriever(search_kwargs={"k": 4}) 
