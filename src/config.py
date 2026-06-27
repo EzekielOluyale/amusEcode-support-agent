ae0f42c5353cbb0812fd3da0e2f073dc51e31f04
Oluyale Ezekiel <amusecode@gmail.com>
2026-06-27 05:21:37 +0100

    fix: added load_dotenv()

--- src/config.py
+++ src/config.py
@@ -1,25 +1,20 @@
-import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from pinecone import Pinecone
 from langchain_pinecone import PineconeVectorStore
 
-api_key = os.environ.get("GEMINI_API_KEY") or os.environ.get("GOOGLE_API_KEY")
-
-if not api_key:
-    raise ValueError("CRITICAL ERROR: No API Key found in environment variables.")
+# Load environment variables globally
+load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=0,
-    google_api_key=api_key,
-    vertexai=True
+    vertexai=True,
+    temperature=0
     )
 
 # Vector Database & Retriever
 embeddings = GoogleGenerativeAIEmbeddings(
     model="text-embedding-004",
-    google_api_key=api_key,
     vertexai=True
 )
 
